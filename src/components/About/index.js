326c016bbf68a3dbe7657f129cd7bd518b663f44
rharris529 <jordandine@gmail.com>
2022-07-13 14:27:59 -0400

    added social media links

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,6 +8,10 @@ function About() {
             I'm a new full stack developer now based in the Philadelphia. Before becoming a developer, I worked in education/youth programming. Eternally curious, I'm excited to merge this new field with my passions! 
 
             Check out some stuff I've worked on! 
+
+            Here are my socials: 
+            <a href='https://github.com/rharris529'>Github</a>
+            <a href='https://www.linkedin.com/in/ryan-harris-23b9b3136/'>LinkedIn</a>
             </p>
         </div>
     );
