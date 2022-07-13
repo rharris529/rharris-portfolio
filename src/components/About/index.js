7f63d7a83c3e1c973c8c2b1531ed608e106d8846
rharris529 <jordandine@gmail.com>
2022-07-12 20:50:46 -0400

    added a bio

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1 id="about">About Me</h1>
+            <p>
+            I'm a new full stack developer now based in the Philadelphia. Before becoming a developer, I worked in education/youth programming. Eternally curious, I'm excited to merge this new field with my passions! 
+
+            Check out some stuff I've worked on! 
+            </p>
+        </div>
+    );
+}
+
+export default About;
